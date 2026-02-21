e3dcf688e8188afebe057dfe613088b52f0c4980
Arnab Saha <arnabsaha2506@gmail.com>
2026-02-21 13:07:29 +0530

    First commit

---
+++ src/main/java/com/example/EComOrder/EComOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.EComOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EComOrder/EComOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EComOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
